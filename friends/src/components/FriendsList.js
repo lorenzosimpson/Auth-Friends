cc3dece05740e3b110deb3cdbb7b62da40291c0f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-15 16:49:19 -0400

    Friend names displaying

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+import Friend from './Friend';
+
 
 const FriendsList = () => {
     const [friends, setFriends] = useState([])
 
     useEffect(() => {
         axiosWithAuth()
-        .get('/api/data')
+        .get('/api/friends')
         .then(res => {
             console.log(res)
             setFriends(res.data)
 
         })
+        .catch(err => console.log(err))
     }, [])
     
     return (
         <div className='friends-list'>
-            <h1>hello from friends list</h1>
+            {friends.map(friend => (
+                <Friend name={friend.name} key={friend.id}/>
+            ))}
         </div>
     )
 }
