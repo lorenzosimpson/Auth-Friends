746faaebc5c6736dbae422bed5e40d896b77b7df
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-15 17:39:45 -0400

    completes mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 // Import components
 import NavBar from './components/NavBar';
